5db8b4d3d4f7e594f31b9f027a357c27a2da9fcd
BAEKJlHYE <bjiekie@gmail.com>
2025-05-04 18:44:16 +0900

    change chat prefix

--- src/main/java/jiekie/util/ChatUtil.java
+++ src/main/java/jiekie/util/ChatUtil.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Player;
 
 public class ChatUtil {
     public static String getWarnPrefix() {
-        return "[ " + ChatColor.YELLOW + "❗" + ChatColor.WHITE + " ] ";
+        return "\uA003 ";
     }
 
     public static void notPlayer(CommandSender sender) {
@@ -18,6 +18,7 @@ public class ChatUtil {
     }
 
     public static void showColorCode(Player player) {
+        player.sendMessage("");
         player.sendMessage(getWarnPrefix() + "색코드 목록");
         player.sendMessage("　　　" + ChatColor.BLACK + "&0" + ChatColor.DARK_BLUE + "　&1" + ChatColor.DARK_GREEN + "　&2" + ChatColor.DARK_AQUA + "　&3" + ChatColor.DARK_RED + "　&4" + ChatColor.DARK_PURPLE + "　&5");
         player.sendMessage("　　　" + ChatColor.GOLD + "&6" + ChatColor.GRAY + "　&7" + ChatColor.DARK_GRAY + "　&8" + ChatColor.BLUE + "　&9" + ChatColor.GREEN + "　&a" + ChatColor.AQUA + "　&b");
@@ -25,5 +26,6 @@ public class ChatUtil {
         player.sendMessage("");
         player.sendMessage("　　　" + ChatColor.BOLD + "&l" + ChatColor.RESET + "　" + ChatColor.STRIKETHROUGH + "&m" + ChatColor.RESET + "　" + ChatColor.UNDERLINE + "&n" + ChatColor.RESET + ChatColor.ITALIC + "　&o" + ChatColor.RESET + ChatColor.RESET + "　&r");
         player.sendMessage("　　　" + "&k : magic");
+        player.sendMessage("");
     }
 }
